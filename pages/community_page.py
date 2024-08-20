c44fc00899cfb639bab9d15a9446b3f025bf826f
unknown <nandinisarkar079@gmail.com>
2024-08-19 21:27:50 -0400

    task_7_connect_the_company_page

--- pages/community_page.py
+++ pages/community_page.py
@@ -6,7 +6,6 @@ class CommunityPage(Page):
     COMMUNITY_BUTTON = (By.XPATH, "//div[@class='setting-text' and text()='Community']")
     VERIFY_CONTACT_SUPPORT = (By.XPATH, "//a[@href='/notification' and text()='Contact support']")
     CLICK_CONTACT_SUPPORT = (By.XPATH, "//a[@id='w-node-_746955f8-6d08-524d-7f43-03daaced30ff-7f66deba' and text()='Contact support']")
-    #NEW_CONTACT_PAGE = 'https://t.me/reelly_dubai_bot?start=w18415667'
 
     def click_community_option(self):
         self.wait_until_clickable_click(*self.COMMUNITY_BUTTON)
--- pages/connect_the_company_page.py
+++ pages/connect_the_company_page.py
@@ -8,9 +8,9 @@ class ConnectTheCompanyPage(Page):
     INSTAGRAM_LINK = (By.XPATH, "//div[@class='text-social' and text()= 'Instagram']")
     TELEGRAM_LINK = (By.XPATH, "//div[@class='text-social' and text()= 'Telegram']")
     LINKEDIN_LINK = (By.XPATH, "//div[@class='text-social' and text()= 'LinkedIn']")
-    WHATSAPP_LINK = (By.XPATH, "//div[@class='text-social' and text()= 'Youtube']")
+    YOUTUBE_LINK = (By.XPATH, "//div[@class='text-social' and text()= 'Youtube']")
     VERIFY_COMPANY_PAGE = (By.XPATH, "//div[@class='get-free-period menu' and text()= 'Connect the company']")
-    #CLICK_COMPANY_PAGE =(By.XPATH, "//*[text()= 'Connect the company']")
+
     def click_contact_us(self):
         self.wait_until_clickable_click(*self.CLICK_CONTACT)
 
@@ -20,15 +20,14 @@ class ConnectTheCompanyPage(Page):
     def verify_social_media_icons(self):
         self.wait_until_visible(*self.INSTAGRAM_LINK)
         self.verify_text('Instagram', *self.INSTAGRAM_LINK)
-        self.wait_until_visible(*self.INSTAGRAM_LINK)
+        self.wait_until_visible(*self.TELEGRAM_LINK)
         self.verify_text('Telegram', *self.TELEGRAM_LINK)
-        self.wait_until_visible(*self.INSTAGRAM_LINK)
+        self.wait_until_visible(*self.LINKEDIN_LINK)
         self.verify_text('LinkedIn', *self.LINKEDIN_LINK)
-        self.wait_until_visible(*self.INSTAGRAM_LINK)
-        self.verify_text('Youtube', *self.WHATSAPP_LINK)
+        self.wait_until_visible(*self.YOUTUBE_LINK)
+        self.verify_text('Youtube', *self.YOUTUBE_LINK)
 
-
-    def verify_connect_the_company_button_availabe_and_clickable(self):
+    def verify_connect_the_company_button_available_and_clickable(self):
         all_listing_icons = self.find_elements(*self.VERIFY_COMPANY_PAGE)
 
         for listing in all_listing_icons:
